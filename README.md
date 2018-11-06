a7270b71287135e8ca2dbe17e728e37256d4da1b
Huu Nguyen <huu.nguyen@gmail.com>
2018-11-05 20:04:08 -0500

    Final submission

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 Objective:
+For given API (http://oscars.yipitdata.com)
+
+1. Print Year-Title-Budget combination for all winners
+2. Print Average Budget for all winners (Assuming to print to console)
 
 Approach:
+
+1. Get initial dataset from API
+2. Extract winner and create new array of objects with Year, Title, and Budget
+   a. Year clean up data (first 4 characters repreent year)
+   b. Clean film name up to exclude brackets at the end
+   c. Make GET request to get detail URL info for budget
+   1. If missing, will be undefined
+   2. Take out extra [], (), and leading US in data
+   3. Save infromation as raw Budget
+   4. Convert raw budget to US and account for inflation (gives options for both)
+      -assume 2% annual inflation rate to 2018 dollars
+      -assumed conversation of £ to $ of 1.3
+      -account for those that are listed in millions
+      -if range is provided, find average for range
+      -push object into final array to console and .json file
+      -Print Average with inflation and without to console. Exclude movies with no budget data from average
 
 Setup Instructions:
 
@@ -8,3 +28,6 @@ Setup Instructions:
 2. Type "npm run start"
 
 Average budget:
+Total with budget info 82
+Avg budget with Inflation $29,336,682.19 in 2018 dollars
+Avg budget with No Inflation $17,243,245.85
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const fs = require('fs')
 
 const getData = async address => {
   try {
@@ -37,8 +38,71 @@ const budgetClean = budgetData => {
   return budgetData
 }
 
+const budgetConvert = (year, rawBudget) => {
+  //Assumption if two numbers are given in different currency, first number taken
+  let currencyConversion = 1 //default currency is USD
+  let factor = 1 //default value is not in millions
+  const inflation = 1.02 //assumed 2% inflation rate
+  const years = 2018 - year //brining to 2018 dollars based on assumed inflation
+  const totalInflation = Math.pow(inflation, years)
+  if (rawBudget === undefined) {
+    return rawBudget
+  }
+  if (rawBudget[0] === '£') {
+    currencyConversion = 1.3 //current pound to USD
+  } else if (rawBudget[0] === '€') {
+    currencyConversion = 1.14 // idn't see any € but going to add this in case
+  }
+  if (rawBudget.includes('million')) {
+    factor = 1000000
+  }
+  //now need to parse for the numbers
+  if (rawBudget.includes('million')) {
+    rawBudget = rawBudget.substring(1, rawBudget.indexOf('m')).trim()
+    if (rawBudget.includes('-')) {
+      //accounts for ranges, will find the average for ranges
+      let range = rawBudget.split('-')
+      rawBudget = ((range[0] * 1.0 + range[1] * 1.0) / 2.0).toString()
+    } else if (rawBudget.includes('–')) {
+      let range = rawBudget.split('–') //different symbol for godfather?
+      rawBudget = ((range[0] * 1.0 + range[1] * 1.0) / 2.0).toString()
+    }
+  } else {
+    rawBudget = parseFloat(
+      rawBudget
+        .substring(1)
+        .trim()
+        .replace(/,/g, '')
+    ) //removes commas
+  }
+  return {
+    year,
+    rawBudget,
+    currencyConversion,
+    factor,
+    totalInflation,
+    finalBudget: rawBudget * currencyConversion * factor * totalInflation,
+    finalBudgetNoInflation: rawBudget * currencyConversion * factor,
+  }
+}
+
+//options for currency formatting for output
+const opts = '{style: "decimal", currency: "USD", minimumFractionDigits: 2}'
+
+const writeData = data => {
+  const writeStream = fs.createWriteStream('oscarResults.json')
+  writeStream.write(data, 'utf8')
+  writeStream.on('finish', () => {
+    console.log('Data done!')
+  })
+  writeStream.end()
+}
+
 const oscarResults = async () => {
   const initialData = await getData('http://oscars.yipitdata.com')
+  let sumBudgetInflation = 0
+  let sumBudgetNoInflation = 0
+  let countWithoutNull = 0
   //filtering data for winners and creating an object for each year
   const oscarWinners = await Promise.all(
     initialData.results.map(async yearlyData => {
@@ -52,14 +116,44 @@ const oscarResults = async () => {
       //Dealing with budget
       const additionalInfo = await getData(winner[0]['Detail URL'])
       winnerOutput.rawBudget = budgetClean(additionalInfo.Budget)
+      //debugging if we need to write to output dataset
+      // winnerOutput.convertedData = budgetConvert(
+      //   winnerOutput.year,
+      //   winnerOutput.rawBudget
+      // )
+      const convertedData = budgetConvert(
+        winnerOutput.year,
+        winnerOutput.rawBudget
+      )
+      if (winnerOutput.rawBudget) {
+        countWithoutNull++
+        sumBudgetInflation = sumBudgetInflation + convertedData.finalBudget
+        sumBudgetNoInflation =
+          sumBudgetNoInflation + convertedData.finalBudgetNoInflation
+        winnerOutput.budgetInflation =
+          '$' + convertedData.finalBudget.toLocaleString('en-US', opts)
+        winnerOutput.budgetNoInflation =
+          '$' +
+          convertedData.finalBudgetNoInflation.toLocaleString('en-US', opts)
+      }
+      //writing inflation and non inflation to final dataset
+
       return winnerOutput
     })
   )
-  const budget = oscarWinners.map(el => {
-    return el.rawBudget
-  })
-  console.log(budget)
-  return oscarWinners
+  await console.log('Output dataset', oscarWinners)
+  await console.log('Total with budget info', countWithoutNull)
+  await console.log(
+    'Avg budget with Inflation',
+    '$' + (sumBudgetInflation / countWithoutNull).toLocaleString('en-US', opts)
+  )
+  await console.log(
+    'Avg budget with No Inflation',
+    '$' +
+      (sumBudgetNoInflation / countWithoutNull).toLocaleString('en-US', opts)
+  )
+
+  writeData(JSON.stringify(oscarWinners))
 }
 
 oscarResults()
